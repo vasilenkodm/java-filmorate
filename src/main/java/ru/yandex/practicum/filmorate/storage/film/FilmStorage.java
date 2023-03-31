225699648514f31d3ccff10c57dc50058eba1171
VaDm <vasilenko.dm@yandex.ru>
2023-03-31 16:55:34 +0500

    DV add-most-populars 12-4

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -2,15 +2,16 @@ package ru.yandex.practicum.filmorate.storage.film;
 
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.storage.ItemStorage;
-import ru.yandex.practicum.filmorate.type.DirectorIdType;
-import ru.yandex.practicum.filmorate.type.FilmIdType;
-import ru.yandex.practicum.filmorate.type.FilmsByDirectorSortByMode;
-import ru.yandex.practicum.filmorate.type.UserIdType;
+import ru.yandex.practicum.filmorate.type.*;
 
 import java.util.List;
 
 public interface FilmStorage extends ItemStorage<FilmIdType, Film> {
-    List<Film> getPopular(int maxCount);
+    List<Film> getPopular(int maxCount, GenreIdType genreId, Integer year);
+
+    default List<Film> getPopular(int maxCount) {
+        return getPopular(maxCount, null, null);
+    }
 
     int getLikesCount(FilmIdType id);
 
