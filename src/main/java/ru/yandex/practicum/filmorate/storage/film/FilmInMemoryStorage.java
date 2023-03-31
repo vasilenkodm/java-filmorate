0bd04795c4bb74a19555c74a981d89911d4cf95d
ArtakJava <artakmatevosian@yandex.ru>
2023-03-31 19:12:27 +0400

    feature: add-recommendations

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmInMemoryStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmInMemoryStorage.java
@@ -98,4 +98,9 @@ public class FilmInMemoryStorage extends BaseItemInMemoryStorage<FilmIdType, Fil
         throw new FeatureNotSupportedException(this.getClass(), log);
     }
 
+    @Override
+    public List<Film> getRecommendations(UserIdType userId) {
+        return null;
+    }
+
 }
